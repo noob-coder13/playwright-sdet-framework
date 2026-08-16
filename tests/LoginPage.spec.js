fe1f4c279a25bc5afbf2f29169b53a02dbbcf687
GeneralTitus03 <abhishek3rawat@gmail.com>
2026-08-16 20:09:03 +0530

    test: add data-driven login tests using external test data

--- tests/LoginPage.spec.js
+++ tests/LoginPage.spec.js
@@ -2,24 +2,24 @@ import {test, expect} from '../fixtures/base.fixture';
 import { users } from '../utils/test-data';
 
 
-// test('login test successful', async ({loginPage, page})=>{
+test('login test successful', async ({loginPage, page})=>{
     
-//     await loginPage.goto();
-//     await loginPage.login('standard_user', 'secret_sauce');
-//     await expect(loginPage.homepageHeader).toBeVisible();
-//     await expect(page).toHaveURL(/inventory.html/);
+    await loginPage.goto();
+    await loginPage.login('standard_user', 'secret_sauce');
+    await expect(loginPage.homepageHeader).toBeVisible();
+    await expect(page).toHaveURL(/inventory.html/);
 
-// });
+});
 
-// test('Failed login scenario', async({loginPage})=>{
-//     await loginPage.goto();
-//     await loginPage.login('standard_user', 'secret_sauc');
-//     const errorMessage=  await loginPage.getErrorMessage();
-//     expect(errorMessage).toContain("Epic sadface: Username and password do not match any user in this service");
+test('Failed login scenario', async({loginPage})=>{
+    await loginPage.goto();
+    await loginPage.login('standard_user', 'secret_sauc');
+    const errorMessage=  await loginPage.getErrorMessage();
+    expect(errorMessage).toContain("Epic sadface: Username and password do not match any user in this service");
 
-// });
+});
 
-test.only('login with test data', async ({loginPage, page})=>{
+test('login with test data', async ({loginPage, page})=>{
     const login= Object.keys(users);
     for(const value of login){
         await loginPage.goto();
