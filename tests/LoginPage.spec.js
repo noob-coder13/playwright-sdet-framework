c67da3f2adedf0c3167ae69e471daf899e3e6165
GeneralTitus03 <abhishek3rawat@gmail.com>
2026-08-16 20:07:44 +0530

    test: add data-driven login tests using external test data

--- tests/LoginPage.spec.js
+++ tests/LoginPage.spec.js
@@ -1,19 +1,32 @@
 import {test, expect} from '../fixtures/base.fixture';
+import { users } from '../utils/test-data';
 
 
-test('login test successful', async ({loginPage, page})=>{
+// test('login test successful', async ({loginPage, page})=>{
     
-    await loginPage.goto();
-    await loginPage.login('standard_user', 'secret_sauce');
-    await expect(loginPage.homepageHeader).toBeVisible();
-    await expect(page).toHaveURL(/inventory.html/);
+//     await loginPage.goto();
+//     await loginPage.login('standard_user', 'secret_sauce');
+//     await expect(loginPage.homepageHeader).toBeVisible();
+//     await expect(page).toHaveURL(/inventory.html/);
 
-});
+// });
 
-test('Failed login scenario', async({loginPage})=>{
-    await loginPage.goto();
-    await loginPage.login('standard_user', 'secret_sauc');
-    const errorMessage=  await loginPage.getErrorMessage();
-    expect(errorMessage).toContain("Epic sadface: Username and password do not match any user in this service");
+// test('Failed login scenario', async({loginPage})=>{
+//     await loginPage.goto();
+//     await loginPage.login('standard_user', 'secret_sauc');
+//     const errorMessage=  await loginPage.getErrorMessage();
+//     expect(errorMessage).toContain("Epic sadface: Username and password do not match any user in this service");
 
-});
+// });
+
+test.only('login with test data', async ({loginPage, page})=>{
+    const login= Object.keys(users);
+    for(const value of login){
+        await loginPage.goto();
+        await loginPage.login(users[value].username, users[value].password);
+        if(users[value].shouldSucceed){
+            await expect(page).toHaveURL(/inventory.html/);}
+        else{
+            await expect(await loginPage.getErrorMessage()).toContain(users[value].errorMessage);}
+        
+}});
--- utils/test-data.js
+++ utils/test-data.js
@@ -1,6 +1,6 @@
 export const users= {
-    standard: {username: 'standard_user', password: 'secret_sauce'},
-    lockedOut: {username: 'lockedout_user', password: 'secret_sauce'},
-    problem :{username: 'problem_user', password:'secret_sauce'},
-    performanceGlitch: {username: 'performance_glitch_user', password: 'secret_sauce'}
+    standard: {username: 'standard_user', password: 'secret_sauce', shouldSucceed: true, errorMessage: null},
+    lockedOut: {username: 'lockedout_user', password: 'secret_sauce', shouldSucceed: false, errorMessage: 'Epic sadface: Username and password do not match any user in this service'},
+    problem :{username: 'problem_user', password:'secret_sauce', shouldSucceed: true, errorMessage: null},
+    performanceGlitch: {username: 'performance_glitch_user', password: 'secret_sauce', shouldSucceed: true, errorMessage: null}
 };
